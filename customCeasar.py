a41729b862e17cc7f471d020c8deec367687c080
luke <lukebunge@live.com>
2020-09-17 14:50:49 -0400

    Commented

--- customCeasar.py
+++ customCeasar.py
@@ -1,7 +1,10 @@
-alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789.$%/!@#'
+# A custom Caesar cipher who's key is different based on odd/even index (+2 on even)
 
+alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789.$%/!@#'
 message = 'j  n332zvp  4p03rzw  zo  4qp  lln3j2  ltysn2  5tuw  wz2  xjvn  4qp  lpj3n2  ltysn2  jy7 3nn32n   zwnn   4qp   j42llvn2   otwo   x52   lkz34   2sn   yn7   ltysn2   33ryp   2n6n21p nyptwpn2ryp lwo l2702lwlu91t1'
 
+# Function takes some message, a key, and whether or not you want to decrypt or encrypt the message
+# Key gets 2 added on even index
 def ceasar(input, key, mode):
     output = ''
     loop = 1
